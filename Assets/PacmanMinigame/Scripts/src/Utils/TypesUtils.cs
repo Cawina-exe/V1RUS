22f0a47969b66c756c0378ec4928b90f11d5f034
Pedro Silva <uc2023235452@student.uc.pt>
2025-11-26 23:15:14 +0000

    Added TypesUtil class

---
+++ Assets/PacmanMinigame/Scripts/src/Utils/TypesUtils.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pacman.Utils
+{
+    /// <summary>
+    /// Utility class defining core types and constants for the grid environment.
+    /// Translates 'types_utils.py' to C#.
+    /// </summary>
+    public static class TypesUtils
+    {
+        // Percepts infered from Environment
+        public class Percept : Dictionary<Vector2Int, string> { }
+
+        // Move/Direction Definitions
+        public static readonly Dictionary<string, Vector2Int> MOVES = new Dictionary<string, Vector2Int>
+        {
+            { "UP",    Vector2Int.up },    // (0, 1)
+            { "DOWN",  Vector2Int.down },  // (0, -1)
+            { "LEFT",  Vector2Int.left },  // (-1, 0)
+            { "RIGHT", Vector2Int.right }, // (1, 0)
+            { "WAIT",  Vector2Int.zero }   // (0, 0)
+        };
+
+        // DIRECTIONS = list(MOVES.keys())
+        public static readonly List<string> DIRECTIONS = new List<string>(MOVES.Keys);
+    }
+}
